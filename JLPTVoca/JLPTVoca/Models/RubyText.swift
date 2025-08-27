98867a3700cf76ab80acd1e12356caa713e618ed
Rama <gudrms2538@gmail.com>
2025-08-27 15:10:51 +0900

    feat: 한자 단어 상단 Furigana 표시 구현

---
+++ JLPTVoca/JLPTVoca/Models/RubyText.swift
@@ -0,0 +1,14 @@
+
+//
+//  RubyText.swift
+//  JLPTVoca
+//
+//  Created by Rama on 8/27/25.
+//
+
+import Foundation
+
+struct RubyText: Codable, Hashable {
+    let text: String
+    let furigana: String?
+}
--- JLPTVoca/JLPTVoca/Models/Word.swift
+++ JLPTVoca/JLPTVoca/Models/Word.swift
@@ -9,32 +9,51 @@ import Foundation
 import SwiftData
 
 @Model
-final class Word {
-   var id: UUID
-   var japanese: String
-   var korean: String
-   var furigana: String
-   var isFavorite: Bool
-   var jlptLevel: Int
-   var maturityState: Int
-   var nextReviewDate: Date
-   
-   init(
-       japanese: String,
-       korean: String,
-       furigana: String,
-       jlptLevel: Int,
-       isFavorite: Bool = false,
-       maturityState: Int = 0,
-       nextReviewDate: Date = Date()
-   ) {
-       self.id = UUID()
-       self.japanese = japanese
-       self.korean = korean
-       self.furigana = furigana
-       self.jlptLevel = jlptLevel
-       self.isFavorite = isFavorite
-       self.maturityState = maturityState
-       self.nextReviewDate = nextReviewDate
-   }
+final class Word: Decodable {
+    var id: UUID
+    var japanese: [RubyText]
+    var korean: String
+    var isFavorite: Bool
+    var jlptLevel: Int
+    var maturityState: Int
+    var nextReviewDate: Date
+    
+    var plainJapanese: String {
+        japanese.map { $0.text }.joined()
+    }
+    
+    init(
+        japanese: [RubyText],
+        korean: String,
+        jlptLevel: Int,
+        isFavorite: Bool,
+        maturityState: Int,
+        nextReviewDate: Date
+    ) {
+        self.id = UUID()
+        self.japanese = japanese
+        self.korean = korean
+        self.jlptLevel = jlptLevel
+        self.isFavorite = isFavorite
+        self.maturityState = maturityState
+        self.nextReviewDate = nextReviewDate
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.japanese = try container.decode([RubyText].self, forKey: .japanese)
+        self.korean = try container.decode(String.self, forKey: .korean)
+        self.jlptLevel = try container.decode(Int.self, forKey: .jlptLevel)
+        
+        self.id = UUID()
+        self.isFavorite = false
+        self.maturityState = 0
+        self.nextReviewDate = Date()
+    }
+}
+
+extension Word {
+    enum CodingKeys: String, CodingKey {
+        case japanese, korean, jlptLevel
+    }
 }
