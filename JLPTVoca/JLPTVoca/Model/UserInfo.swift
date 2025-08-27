22d2cd4aed5d760c9b57b0a421de892fefb75291
Rama <gudrms2538@gmail.com>
2025-08-27 17:43:23 +0900

    chore: 폴더링 변경

---
+++ JLPTVoca/JLPTVoca/Model/UserInfo.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfo.swift
+//  JLPTVoca
+//
+//  Created by Rama on 8/27/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class UserSettings {
+    var targetExamLevel: Int
+    var targetExamDate: Date
+    var dailyNewWordGoal: Int
+    
+    init(
+        targetExamLevel: Int,
+        targetExamDate: Date,
+        dailyNewWordGoal: Int
+    ) {
+        self.targetExamLevel = targetExamLevel
+        self.targetExamDate = targetExamDate
+        self.dailyNewWordGoal = dailyNewWordGoal
+    }
+}
