75c6913c0a99e3b3e4ec336825a1539990e390ee
Rama <gudrms2538@gmail.com>
2025-09-01 01:36:52 +0900

    feat: 단어 암기 세션 진행 중 pop을 할 때 alert 화면 구현

--- JLPTVoca/JLPTVoca/Views/WordStudyView.swift
+++ JLPTVoca/JLPTVoca/Views/WordStudyView.swift
@@ -10,8 +10,28 @@ import SwiftUI
 
 struct WordStudyView: View {
     @Environment(WordManager.self) private var wordManager
+    @Environment(NavigationManager<HomeRoute>.self) private var router
+    
+    @State private var showAlert = false
     
     var body: some View {
+        wordDeckView()
+            .navigationBarBackButtonHidden(true)
+            .toolbar(.hidden, for: .tabBar)
+            .toolbar { customBackButton() }
+            .alert(
+                "학습 종료",
+                isPresented: $showAlert,
+                actions: {
+                    alertButtons
+                }, message: {
+                    Text("7년 연습생 하고 집에 갈래?")
+                })
+    }
+}
+
+extension WordStudyView {
+    private func wordDeckView() -> some View {
         ZStack {
             ForEach(wordManager.wordDeck) { word in
                 WordCardView(
@@ -26,6 +46,25 @@ struct WordStudyView: View {
                     )
                 }
             }
+        }
+    }
+    
+    @ToolbarContentBuilder
+    private func customBackButton() -> some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button {
+                showAlert = true
+            } label: {
+                Image(systemName: "chevron.backward")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var alertButtons: some View {
+        Button("취소", role: .cancel) { }
+        Button("종료", role: .destructive) {
+            router.pop()
         }
     }
 }
