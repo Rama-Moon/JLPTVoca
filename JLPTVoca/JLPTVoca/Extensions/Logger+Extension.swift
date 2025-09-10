9175a9ac596ca123e78f5f7bc2c75d9d01bb6932
Rama <gudrms2538@gmail.com>
2025-09-11 00:43:03 +0900

    add: Logger, UnitTest 파일 추가

---
+++ JLPTVoca/JLPTVoca/Extensions/Logger+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Logger+Extension.swift
+//  JLPTVoca
+//
+//  Created by Rama on 9/9/25.
+//
+
+import SwiftUI
+
+struct Logger_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Logger_Extension()
+}
