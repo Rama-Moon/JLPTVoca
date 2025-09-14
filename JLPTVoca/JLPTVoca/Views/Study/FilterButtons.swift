ea3a626240e3de0779d3866417e1b9fe7707ce6b
Rama <gudrms2538@gmail.com>
2025-09-15 00:59:33 +0900

    fix: 모델링에 한국어, 일본어 예시 프로퍼티 추가

--- JLPTVoca/JLPTVoca/Views/Study/FilterButtons.swift
+++ JLPTVoca/JLPTVoca/Views/Study/FilterButtons.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct FilterButtons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 15) {
+            filterButton(text: "학습할게요")
+            filterButton(text: "알고 있어요")
+        }
     }
-}
-
-#Preview {
-    FilterButtons()
+    
+    func filterButton(text: String) -> some View {
+        Button(action: { }){
+            Text("\(text)")
+                .font(.b3)
+                .foregroundStyle(Color.black100)
+                .padding(.horizontal, 45)
+                .padding(.vertical, 25)
+                .background(Color.white40)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+    }
 }
